49f97f0c36ba37682b09a493578711bce25b1882
Eric Minh Truong <ermtruon@ucsc.edu>
2022-03-01 18:53:19 +0000

    Server Main can handle multiple MQTT connections

--- software/server/src/server_main.py
+++ software/server/src/server_main.py
@@ -1,29 +1,79 @@
 import sqlite3
 from sqlite_functions import *
 from datetime import datetime
+import paho.mqtt.client as mqtt
 
+def on_message(client, userdata, message):
+    print("Received message: ", str(message.payload.decode("utf-8")))
+
+
+######################################################
+#                   INITIALIZE
+######################################################
+# Constants
 DATA_DIR = '../data/'
 DB_NAME = 'data.sqlite'
+mqttBroker = "broker.hivemq.com"
 
-# connection = sqlite3.connect(DATA_DIR + DB_NAME)
-connection = sqlite3.connect(":memory:") # DEBUG
+# SQLite
+# connection = sqlite3.connect(DATA_DIR + DB_NAME)  #TODO Uncomment to use an actual database
+connection = sqlite3.connect(":memory:")            #TODO Remove this DEBUG line
 cursor = connection.cursor()
-
 db_init(cursor)
-new_plug = Plug("prototype")
-db_add_plug(cursor, new_plug)
-connection.commit()
+
+# MQTT
+client = mqtt.Client("Server")
+client.connect(mqttBroker)
+client.loop_start()  # Runs a loop in a background thread
+client.subscribe("plug/#")
+client.on_message=on_message
 
 
-cursor.execute("SELECT * FROM Plugs WHERE name=(:name)", {'name': new_plug.name})
-print(cursor.fetchall())
+######################################################
+#             Close Server on Command
+######################################################
+# Closing Command
+running = True
+while (running):
+    command = input("Type 'stop' to close the server\n\n")
+    running = ("stop" != command)
+    
+# Graceful Close
+client.loop_stop()  # MQTT
+connection.commit() # SQLite
+connection.close()  # SQLite
 
-# Testing Datapoints
-example_plug_id = 1
-db_add_data(cursor, Datapoint(datetime.now(), example_plug_id, 420.69))
-cursor.execute("SELECT * from Data WHERE plug_id=(:plug_id)", {'plug_id': example_plug_id})
-print(cursor.fetchall())
 
-connection.commit()
+######################################################
+#                 Message Handling
+######################################################
+def on_message(client, userdata, message):
+    print("Received message: ", str(message.payload.decode("utf-8")))
 
-connection.close()  # Good practice to close
+
+
+
+
+
+
+
+######################################################
+#                   DEBUG ZONE
+######################################################
+# new_plug = Plug("prototype")
+# db_add_plug(cursor, new_plug)
+# connection.commit()
+
+
+# cursor.execute("SELECT * FROM Plugs WHERE name=(:name)", {'name': new_plug.name})
+# print(cursor.fetchall())
+
+# # Testing Datapoints
+# example_plug_id = 1
+# db_add_data(cursor, Datapoint(datetime.now(), example_plug_id, 420.69))
+# cursor.execute("SELECT * from Data WHERE plug_id=(:plug_id)", {'plug_id': example_plug_id})
+# print(cursor.fetchall())
+
+# connection.commit()
+
+# connection.close()  # Good practice to close
