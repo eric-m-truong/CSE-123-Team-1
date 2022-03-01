cd8214414affe17b72cd683cb7d8679d7f377b68
Eric Minh Truong <ermtruon@ucsc.edu>
2022-03-01 18:02:47 +0000

    MQTT Test server

---
+++ software/server/src/mqtt_server_test.py
@@ -0,0 +1,23 @@
+# Reference:
+# https://medium.com/python-point/mqtt-basics-with-python-examples-7c758e605d4
+
+import paho.mqtt.client as mqtt
+import datetime
+import time
+
+def on_message(client, userdata, message):
+    print("Received message: ", str(message.payload.decode("utf-8")))
+
+mqttBroker = "broker.hivemq.com"
+
+client = mqtt.Client("Server")
+client.connect(mqttBroker)
+
+client.loop_start()  # Runs a loop in a background thread
+
+client.subscribe("plug/#")
+client.on_message=on_message
+
+# DEBUG
+time.sleep(30)
+client.loop_stop()
