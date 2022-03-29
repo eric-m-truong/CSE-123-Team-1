500b1a5b5b1c3af0fc27063c4a65d04e10257b19
Eric Minh Truong <ermtruon@ucsc.edu>
2022-03-29 15:07:06 +0000

    Python testClient now has command line arguments.
    This makes it slightly more convenient to re-run the test client.

--- software/client/testClient.py
+++ software/client/testClient.py
@@ -5,14 +5,25 @@ import paho.mqtt.client as mqtt
 from random import randrange, uniform
 from datetime import datetime
 import time
+import sys
 
-mqttBroker = input("Broker IP: ")
-client_id = input("Enter an ID: ")
+# Arguments
+argc = len(sys.argv)
+if (argc < 4):
+    print("Usage:", sys.argv[0], "<broker_ip> <client_id> <mqtt_topic>")
+    sys.exit(-1)
+
+# mqttBroker = input("Broker IP: ")
+# client_id = input("Enter an ID: ")
+
+mqttBroker = sys.argv[1]
+client_id = sys.argv[2]
+topic = sys.argv[3]
 
 client = mqtt.Client(client_id)  # Initialize
 client.connect(mqttBroker)  # I think this assumes the default port
 
-topic = input("Topic: ")
+# topic = input("Topic: ")
 
 while True:
     randNumber = uniform(20.0, 21.0)
