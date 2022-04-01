863769416d60c01f93334d7cb806c98f72917fea
Eric Minh Truong <ermtruon@ucsc.edu>
2022-04-01 19:43:13 +0000

    Updated the testClient.py to output the updated message payload to the correct topic. Also made a small bash script (runTestClients.sh) to run the testClient.py with parameters.
    The MQTT message payload now just consists of the power (float).
    The testClient.py publishes to the MQTT topic: 'plug/data/<mac_address>'

---
+++ software/client/runTestClients.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+./testClient.py broker.hivemq.com 0025:96FF:FE12:3456
--- software/client/testClient.py
+++ software/client/testClient.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # Code taken and adapted from:
 # https://medium.com/python-point/mqtt-basics-with-python-examples-7c758e605d4
 
@@ -9,24 +11,24 @@ import sys
 
 # Arguments
 argc = len(sys.argv)
-if (argc < 4):
-    print("Usage:", sys.argv[0], "<broker_ip> <client_id> <mqtt_topic>")
+if (argc < 3):
+    print("Usage:", sys.argv[0], "<broker_ip> <mac_address>")
     sys.exit(-1)
 
 mqttBroker = sys.argv[1]
-client_id = sys.argv[2]
-topic = sys.argv[3]
+mac_addr = sys.argv[2]
+topic = "plug/data/" + mac_addr
 
-client = mqtt.Client(client_id)  # Initialize
+client = mqtt.Client()  # Initialize
 client.connect(mqttBroker)  # I think this assumes the default port
 
 while True:
     randNumber = uniform(20.0, 21.0)
-    message = str(datetime.now()) + ", " + client_id + ", " + str(randNumber)
+    message = str(randNumber)
     client.publish(topic, message)
     print(str(topic) + ": " + message)
 
     # DEBUG
-    print("DEBUG:", client._client_id)
+    # print("DEBUG:", client._client_id)
     
     time.sleep(1)
