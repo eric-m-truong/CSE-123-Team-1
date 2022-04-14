ccb0b68c4f4913c7e020859d3167c59b79c35ca4
Mark Gonzales <mgonz165@ucsc.edu>
2022-04-14 14:46:31 -0700

    Eric's broken MQTT integration

--- hardware/MQTTandRelay/MQTTandRelay.ino
+++ hardware/MQTTandRelay/MQTTandRelay.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 #define RELAY 15
 #define SSID_MAX_LEN 32
@@ -6,6 +7,9 @@
 
 const char* ssid = "ESP32-Soft-accessPoint";
 const char* password = "yessirski";
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+
 char wifi_id[SSID_MAX_LEN + 1] = {0};
 char wifi_password[PASS_MAX_LEN + 1] = {0};
 
@@ -63,6 +67,41 @@ void my_strncpy (char* dst, char* src, size_t n) {
   }
 }
 
+/** @brief Tries to connect to WiFi and then an MQTT broker
+ *
+ * @arg ssid	char array that has the WiFi SSID
+ * @arg pass 	char array that has the WiFi password
+ */
+void connect_to_MQTT_broker (char* ssid, char* pass, char* mqtt_broker_ip, int mqtt_broker_port) {
+	WiFiClient wifi_client;
+	PubSubClient mqtt_client(wifi_client);
+	const int MAX_RETRIES = 20;
+	
+	WiFi.begin(ssid, pass);
+	
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.println("Connecting to WiFi...");
+	}
+	
+	Serial.println("Connected to WiFi network: ");
+	Serial.println(ssid);
+	
+	mqtt_client.setServer(mqtt_broker_ip, mqtt_broker_port);
+	
+	while (!mqtt_client.connected()) {
+		Serial.println("Connecting to MQTT Broker...");
+		
+		if (mqtt_client.connect("", "", "")) {
+			Serial.println("Connected!");
+		} else {
+			Serial.print("Failed with state ");
+			Serial.print(mqtt_client.state());
+			delay(2000);
+		}
+	}
+}
+
 void loop() {
 
   WiFiClient client=server.available();
@@ -94,6 +133,8 @@ void loop() {
       Serial.println("Password filled");
       Serial.println(wifi_password);
     }
+	
+	connect_to_MQTT_broker(wifi_id, wifi_password, mqtt_server, mqtt_port);
 
     delay(20);
     client.print(html);
