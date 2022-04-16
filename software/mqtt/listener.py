2e047d07d81403b433b4a660f8d184f5e46dd499
Eric Minh Truong <ermtruon@ucsc.edu>
2022-04-16 07:11:51 +0000

    listener.py payload and broker changes...
    The MQTT payload is now in JSON to make parsing it more readable.
    Listener can grab the topic from the MQTT message.
    Listener subscribes to the topic: 'plux/data/+'
    Listener tries to connect to the unencrypted but authenticated MQTT broker that was setup by @figomez.
    
    NOTE: The username and password on line 25 are not the actual username and password.
          Those are just placeholders. I didn't want to push code with a password in it.
          Check Slack #general for username & password.

--- software/mqtt/listener.py
+++ software/mqtt/listener.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 from sys import exit
 import logging
+import json
 
 
 """
@@ -13,20 +14,22 @@ todo:
 
 def run():
   def on_message(client, userdata, message):
-    ts, pwr = map(float, message.payload.split(b','))
-    logging.debug(f'ts: {ts} pwr: {pwr}')
+    payload = json.loads(message.payload)     # Converts the JSON string to a python dictionary
+    mac_addr = message.topic.split('/').pop() # Grabs the MAC Address from the MQTT topic
+    logging.debug(f'\n ts:  {payload["timestamp"]}\n pwr: {payload["power"]}\n mac: {mac_addr}\n')
 
 
   # init client
   try:
     client = mqtt.Client("listener")
-    client.connect("localhost")
+    client.username_pw_set("user", "pass")  # TODO put the actual username and password here 
+    client.connect("mosquitto.projectplux.info")
     logging.info(f"{__name__}: Connected.")
-    client.subscribe("plug/+")
+    client.subscribe("plux/data/+")
     client.on_message=on_message
     client.loop_forever()
   except ConnectionRefusedError:
-    logging.info(f"No broker running on localhost, exiting...")
+    logging.info(f"No broker running on mosquitto.projectplux.info, exiting...")
     exit(1)
 
 
