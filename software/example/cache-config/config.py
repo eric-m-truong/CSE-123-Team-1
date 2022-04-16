2ac4c20c8b94231fe11d2fb309b8d393ae64031e
Matthew Haley <mhhaley@ucsc.edu>
2022-04-16 15:55:52 -0700

    sw: ex: config defaults

--- software/example/cache-config/config.py
+++ software/example/cache-config/config.py
@@ -5,21 +5,22 @@ from pathlib import Path
 
 CONFIG_PATH = 'config.json'
 
+default = {
+    'broker': {
+        'ip':   'address',
+        'user': 'name',
+        'pass': 'word',
+     },
+}
 
 def init():
   assert not Path(CONFIG_PATH).exists()
-  default = {
-      'broker': {
-          'ip':   'address',
-          'user': 'name',
-          'pass': 'word',
-       },
-  }
   with open(CONFIG_PATH, 'w') as cfg:
     json.dump(default, cfg, indent=2)
 
 
 this = sys.modules[__name__].__dict__
+this.update(default) # ensure there is always a default value
 
 try:
   with open(CONFIG_PATH) as cfg:
--- software/example/cache-config/readme.md
+++ software/example/cache-config/readme.md
@@ -1,5 +1,7 @@
 - reads config once by using fact that module loading is cached
 - assigns contents of config to module namespace
+- ensures expected default values always exist even if user misconfigures
+  `config.json` by merging user config witn a default dict of values
 
 `config.py` prints a message whenever it is loaded.
 
