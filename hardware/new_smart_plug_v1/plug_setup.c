06861343ef93d6e2794fbf686573c33dd8c6b5f8
Anne D Ha <andha@ucsc.edu>
2022-04-19 21:17:07 +0000

    Update plug_setup.c

--- hardware/new_smart_plug_v1/plug_setup.c
+++ hardware/new_smart_plug_v1/plug_setup.c
@@ -1,15 +1,13 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
-#include "ACS712.h"
 #include "time.h"
 #include "plug_setup.h"
 
 // ESP32 Pins
 #define CUR_SENSOR A2                 // adjust pins as necessary
-#define ESP_RELAY 27
+#define MAC_ADDR_LENGTH 17               // length of MAC address in characters (includes colons)
 
-#define MAC_ADDR_LEN 17               // length of MAC address in characters (includes colons)
 
 // MQTT
 const char* mqttDataTopic    = "plux/data/";                  // publishing topic for plug, will be cominbed with MAC address later
@@ -21,19 +19,14 @@ const char* mqttPassword     = "truong";
 
 // Static variables
 //static char mqtt_msg[MAX_TIME_LEN + MAX_MSG];                                           // buffer for MQTT message (timestamp + power)
-static char mac_addr[MAC_ADDR_LEN];                                                     // buffer for MAC address (48 bits/8 = 6 bytes);
-static char mqttDataTopicStr[10 + MAC_ADDR_LEN + 1];                                    // buffer for data topic name
-static char mqttCtrlTopicStr[13 + MAC_ADDR_LEN + 1];
+static char mac_addr[MAC_ADDR_LENGTH];                                                     // buffer for MAC address (48 bits/8 = 6 bytes);
+static char mqttDataTopicStr[10 + MAC_ADDR_LENGTH + 1];                                    // buffer for data topic name
+static char mqttCtrlTopicStr[13 + MAC_ADDR_LENGTH + 1];
 
 // Network credentials
 const char* ssid         = "iPhone";                          // SSID of network to be connected to
 const char* password     = "yaaabruh";                        // password of network to be connected to
 //const char* ssl_server   = "www.howsmyssl.com";             // for encryption: SSL server URL
-const char* ntpServer = "pool.ntp.org";
-
-ACS712  ACS(A2, 5.0, 4095, 66);      // call ACS712.h constructor for 30A variant
-WiFiClient espClient;                // call WiFi constructor
-PubSubClient client(espClient);      // call MQTT constructor
 
 // MQTT setup
 // initialization of MQTT connection
@@ -86,46 +79,8 @@ int network_setup() {
   
 }
 
-// initialization function for ESP32
-// source: https://github.com/RobTillaart/ACS712/blob/master/examples/ACS712_20_AC/ACS712_20_AC.ino
-void setup() { 
-  Serial.begin(115200);                                       // set up baud rate for debugging
-  pinMode(CUR_SENSOR, INPUT);                                 // set CUR_SENSOR to input mode
-  pinMode(ESP_RELAY, OUTPUT);                                     // set RELAY pin to output mode
-  Serial.println(__FILE__);
-  
-  int ret = network_setup();                                  // attempt network connection
-  if (ret) {
-    Serial.println("setup(): unable to connect to network");
-    return;
-  }
-  strncpy(mac_addr, WiFi.macAddress().c_str(), MAC_ADDR_LEN); // save MAC address as string
-  Serial.print("setup(): MAC address - ");
-  Serial.println(WiFi.macAddress());
-  
-  ret = mqtt_setup();                                         // attempt MQTT server connection
-  if (ret) {
-    Serial.println("setup(): unable to connect to MQTT server");
-    return;
-  } else {
-    Serial.print("setup(): publishing to ");
-    Serial.println(mqttDataTopicStr);
-  }
-
-  ret = client.subscribe(mqttCtrlTopicStr, 0);                // subscribe to MQTT control topic
-  if (!ret) {
-    Serial.println("setup(): unable to subscribe to MQTT server topic");
-    return;
-  } else {
-    Serial.print("setup(): subscribed to ");
-    Serial.println(mqttCtrlTopicStr);
-  }
-
-  configTime(-28800, 3600, ntpServer);                        // configure time format for display
-                                                              // -28800: UTC-8
-                                                              // 3600: DST offset
-
-  ACS.autoMidPoint(1);                                        // change this value to refine accuracy
+char* get_mqttCtrlTopicStr(){
+    return mqttCtrlTopicStr;
 }
 
 char* get_mqttDataTopicStr(){
