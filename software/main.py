874c17462357862e091bb067f4bc868ed91e919d
Frank Gomez <figomez@ucsc.edu>
2022-05-05 18:26:33 +0000

    SEVEN RINGS IN HAND

--- software/main.py
+++ software/main.py
@@ -18,6 +18,10 @@ parser.add_argument('--dummy', '-d',
                     action='store_true',
                     default=False,
                     help='Generate dummy plug data.')
+parser.add_argument('--localserver', '-ls',
+                    action='store_true',
+                    default=False,
+                    help='Changes the handler\'s to local server mode by re-enabling the handler.')
 args = parser.parse_args()
 
 format = '%(levelname)s:%(module)s:%(message)s'
@@ -31,13 +35,23 @@ if args.provision:
 # order matters here unfortunately. flask must come last so it isn't the
 # processt the dispatcher waits for. if it is, it will eat one of our
 # KeyboardInterrupt signals.
-es = [lambda: execfn(listener.run),
+if args.localserver:
+  es = [lambda: execfn(listener.run),
+      lambda: execfn(handler.app.run,
+        host='0.0.0.0', # listen on all addresses: accessible outside localhost
+        port=config.webserver['port'],
+        debug=False # don't show python errors in browser on error
+        )
+     ]
+else:
+  es = [lambda: execfn(listener.run),
       #lambda: execfn(handler.app.run,
       #  host='0.0.0.0', # listen on all addresses: accessible outside localhost
       #  port=10080,
       #  debug=False # don't show python errors in browser on error
       #  )
      ]
+
 
 if args.dummy:
   es.insert(1, lambda: execfn(import_module('mqtt.datagen').run))
